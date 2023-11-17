83979a61d2ff78e94c9d7e0c99a6f535435a1a8a
mahendrakale4 <mkale4843@gmail.com>
2023-11-17 12:01:15 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 
+function App() {
+  return (
+    <div >
+
+      HI
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todolist.jsx
@@ -0,0 +1,10 @@
+function Todolist(){
+    return <div>
+        <ul>
+            <li>Task 1</li>
+            <li>Task 2</li>
+            <li>Task 3</li>
+            <li>Task 4</li>
+        </ul>
+    </div>
+}
